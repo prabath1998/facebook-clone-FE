079b0a68884fd0a37d9cc496a0e2c8136cd02508
prabath1998 <prabhathudayanga1998@gmail.com>
2022-07-03 18:44:51 +0530

    bug fixes

--- components/Post.js
+++ components/Post.js
@@ -4,24 +4,24 @@ import { FiThumbsUp } from "react-icons/fi";
 import { FaRegCommentAlt } from "react-icons/fa";
 import { RiShareForwardLine } from "react-icons/ri";
 
-const Post = ({}) => {
+const Post = ({ post }) => {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col" key={post.id}>
       <div className="bg-white mt-6 rounded-md p-4">
         <div className="flex items-center space-x-2">
-          <img src="https://louisem.com/wp-content/uploads/2013/01/3-create-post-link-on-facebook.png" className="rounded-full w-10 h-10" />
+          <img src={post.profilePic} className="rounded-full w-10 h-10" />
           <div>
-            <p className="font-medium">post</p>
-            <p className="text-xs text-gray-500">{new Date().toLocaleString()}</p>
+            <p className="font-medium">{post.name}</p>
+            <p className="text-xs text-gray-500">{post.timeStamp}</p>
           </div>
         </div>
-        <p className="py-4">"post"</p>
+        <p className="py-4">{post.post}</p>
       </div>
-      
+      {post.image != null && (
         <div className="relative h-60 md:h-96 bg-white">
-          <Image src="https://louisem.com/wp-content/uploads/2013/01/3-create-post-link-on-facebook.png" objectFit="cover" layout="fill"></Image>
+          <Image src={post.image} objectFit="cover" layout="fill"></Image>
         </div>
-      
+      )}
       {/* Footer */}
       <div className="flex items-center justify-center bg-white p-2">
         <div className="flex items-center space-x-1 hover:bg-gray-100 flex-grow justify-center p-2 rounded-xl cursor-pointer">
