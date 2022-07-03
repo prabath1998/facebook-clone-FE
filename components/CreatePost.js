fadfbedf05731e7077d5db604d4ee21e1db0fcce
prabath1998 <prabhathudayanga1998@gmail.com>
2022-07-03 11:41:34 +0530

    changes in createpost

--- components/CreatePost.js
+++ components/CreatePost.js
@@ -1,6 +1,9 @@
 import { useSession } from "next-auth/react";
 import Image from "next/image";
 import React from "react";
+import { HiOutlineVideoCamera } from "react-icons/hi";
+import { IoMdPhotos } from "react-icons/io";
+import { BsEmojiSmile } from "react-icons/bs";
 
 const CreatePost = () => {
   const { data: session } = useSession();
@@ -8,15 +11,35 @@ const CreatePost = () => {
     <div className="bg-white rounded-md shadow-md text-gray-500 p-2 ">
       <div className="flex p-4 space-x-2 items-center">
         <Image
-          
           className="rounded-full cursor-pointer"
           src={session?.user.image}
           height={40}
           width={40}
         />
         <form className="flex flex-1">
-            <input className="rounded-full h-12 flex-grow focus:outline-none font-medium bg-gray-100 px-4" type="text" placeholder={`Whats's on your mind, ${session?.user.name.split(" ")[0]}?`}></input>
+          <input
+            className="rounded-full h-12 flex-grow focus:outline-none font-medium bg-gray-100 px-4"
+            type="text"
+            placeholder={`Whats's on your mind, ${
+              session?.user.name.split(" ")[0]
+            }?`}
+          ></input>
+          <button hidden></button>
         </form>
+      </div>
+      <div className="flex justify-evenly py-2">
+        <div className="flex items-center p-1 space-x-1 flex-grow justify-center hover:bg-gray-100 rounded-md hover:cursor-pointer">
+          <HiOutlineVideoCamera size={20} className="text-red-500" />
+          <p className="font-semibold text-gray-600 ">Live Video</p>
+        </div>
+        <div className="flex items-center p-1 space-x-1 flex-grow justify-center hover:bg-gray-100 rounded-md hover:cursor-pointer">
+          <IoMdPhotos size={20} className="text-green-500"/>
+          <p className="font-semibold text-gray-600 ">Photo / Video</p>
+        </div>
+        <div className="flex items-center p-1 space-x-1 flex-grow justify-center hover:bg-gray-100 rounded-md hover:cursor-pointer">
+          <BsEmojiSmile size={20} className="text-yellow-500"/>
+          <p className="font-semibold text-gray-600 ">Feeling / Activity</p>
+        </div>
       </div>
     </div>
   );
